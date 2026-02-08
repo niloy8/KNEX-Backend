0cca60df572e9f33629083b28770769c474472a2
niloy8 <niloybhuiyan688@gmail.com>
2026-02-08 11:29:34 +0600

    Auhtentic issue fixed

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 

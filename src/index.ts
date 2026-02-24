9a91a7995a06cfa809c1d9b5af5dc1e5260b6a4e
niloy8 <niloybhuiyan688@gmail.com>
2026-02-24 13:15:43 +0600

    Fixed image uploading issue

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static('public'));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve uploaded images statically
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
